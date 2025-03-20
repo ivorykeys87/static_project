9959d72b5171a7d18f762bf2ee75b0f82507abb8
github_ivorykeys87 <chase.r.catalano@gmail.com>
2025-03-19 21:42:04 -0400

    Text node and text type enum set up

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from textnode import *
+
+def main():
+    new_text = TextNode("this is a test", TextType.LINK, "https://www.myagencyview.com")
+    print(new_text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
